c7e625b714d9a7aa7aebf334ebddee307711466f
Ztef <Ztef@hotmail.es>
2023-11-25 14:02:22 -0600

    ids duplicados

--- GoogleAuthConfig.js
+++ GoogleAuthConfig.js
@@ -7,7 +7,7 @@ const googleCreds = {
     "private_key_id": process.env.private_key_id,
     "private_key": process.env.private_key,
     "client_email": process.env.client_email,
-    "client_id": process.env.client_id,
+    "client_id": process.env.google_client_id,
     "auth_uri": process.env.auth_uri,
     "token_uri": process.env.token_uri,
     "redirect_uris": process.env.redirect_uris,
